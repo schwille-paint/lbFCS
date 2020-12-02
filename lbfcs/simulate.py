0fad376c927d82c7b5fa65068d455772a383e5da
Florian Stehr <florian.stehr@gmail.com>
2020-12-02 17:51:13 +0100

    Spot&Fit Simulation (2nd): Net-gradient added!

--- lbfcs/simulate.py
+++ lbfcs/simulate.py
@@ -19,6 +19,8 @@ import picasso_addon.autopick as autopick
 
 LOCS_DTYPE = [
     ('frame', 'u4'),
+    ('x_in', 'f4'),
+    ('y_in', 'f4'),
     ('x', 'f4'),
     ('y', 'f4'),
     ('imagers', 'f4'),
@@ -110,7 +112,19 @@ def generate_trace(M,CycleTime,N,koff,kon,c,factor=10):
     return traceN
 
 #%%
-def generate_locs(savepath,reps,M,CycleTime,N,koff,kon,c,box,factor=10):
+def generate_locs(savepath,
+                  reps,
+                  M,
+                  CycleTime,
+                  N,
+                  koff,
+                  kon,
+                  c,
+                  box,
+                  e_tot,
+                  snr,
+                  sigma = 0.9,
+                  use_weight=True):
     
     ### Print statement
     print('%ix simulations of:'%reps)
@@ -120,6 +134,10 @@ def generate_locs(savepath,reps,M,CycleTime,N,koff,kon,c,box,factor=10):
     print('koff: %.1e [1/s]'%koff)
     print('kon:  %.1e [1/Ms]'%kon)
     print('conc: %.1e [M]'%c)
+    print('box: %i [ px]'%box)
+    print('e_tot:  %i [e-]'%e_tot)
+    print('snr: %.1f []'%snr)
+    print()
     
     t0=time.time()
     ### Checks
@@ -134,14 +152,14 @@ def generate_locs(savepath,reps,M,CycleTime,N,koff,kon,c,box,factor=10):
     ### Fill locs
     for r in range(reps):
         ### Generate trace
-        trace = generate_trace(M,CycleTime,N,koff,kon,c,factor)
+        trace = generate_trace(M,CycleTime,N,koff,kon,c,10)
         
         ### Assign
         start=r*M
         end=(r+1)*M
         locs['frame'][start:end] = frames
-        locs['x'][start:end]  = np.ones(M) * np.random.randint(int(box/2),700-int(box/2))
-        locs['y'][start:end]  = np.ones(M) * np.random.randint(int(box/2),700-int(box/2))
+        locs['x_in'][start:end]  = np.ones(M) * np.random.randint(int(box/2),700-int(box/2))
+        locs['y_in'][start:end]  = np.ones(M) * np.random.randint(int(box/2),700-int(box/2))
         locs['imagers'][start:end] = trace
         locs['group'][start:end] = r 
     
@@ -156,52 +174,76 @@ def generate_locs(savepath,reps,M,CycleTime,N,koff,kon,c,box,factor=10):
     ### Remove zeros in imager levels
     locs=locs[locs['imagers']>0]
     
-    ### Add photon noise to photons
-    locs['photons'] = locs['imagers']
+    ### Copy input coordinates
+    locs['x'] = locs['x_in']
+    locs['y'] = locs['y_in']
     
-    ### Convert 
-    # locs=pd.DataFrame(locs)
+    ### Add photon noise by spot creation and fitting
+    locs, spots = noise_fitspots(locs, box, e_tot, snr, sigma, use_weight=True)
+    
+    ### Convert to DataFrame
+    locs = pd.DataFrame(locs)
+    
+    ### Apply photon step filter
+    print('Applying Chung-Kennedy filter ...')
+    tqdm.pandas()
+    locs = locs.groupby('group').progress_apply(lambda df: df.assign( photons_ck = autopick.ck_nlf(df.photons.values).astype(np.float32) ) )
     
     ###
-    # print('Applying Chung-Kennedy filter ...')
-    # tqdm.pandas()
-    # locs = locs.groupby('group').progress_apply(lambda df: df.assign( photons_ck = autopick.ck_nlf(df.photons.values).astype(np.float32) ) )
-    
-    ###
-    # print('Total time: %.2f [s]'%(time.time()-t0))
-    # print()
+    print('Total time: %.2f [s]'%(time.time()-t0))
+    print()
     
     ### Saving
-    # print('Saving ...')
-    # info=[{'reps':reps,
-    #        'Frames':M,
-    #        'exp':CycleTime,
-    #        'N':N,
-    #        'koff':koff,
-    #        'kon':kon,
-    #        'conc':c,
-    #        }]
+    print('Saving ...')
+    info=[{'reps':reps,
+            'Frames':M,
+            'exp':CycleTime,
+            'N':N,
+            'koff':koff,
+            'kon':kon,
+            'conc':c,
+            'box':box,
+            'e_tot':e_tot,
+            'snr':snr,
+            'sigma':sigma,
+            'use_weight':use_weight,
+            }]
     
-    # try:
-    #     addon_io.save_locs(savepath,
-    #                        locs,
-    #                        info,
-    #                        mode='picasso_compatible')
-    # except OSError:
-    #     print('Could not save file to savepath!')
+    try:
+        addon_io.save_locs(savepath,
+                            locs,
+                            info,
+                            mode='picasso_compatible')
+    except OSError:
+        print('Could not save file to savepath!')
         
-    return locs
+    return locs, info, spots 
+
+#%%
+def noise_fitspots(locs, box, e_tot, snr, sigma, use_weight):
+    
+    ### Generate spots
+    spots = generate_spots(locs, box, e_tot, snr, sigma)
+    ### Fit spots
+    locs_noise = fit_spots(locs,box,spots[0],spots[1],use_weight)
+    ### Compute and assing net-gradient
+    print('Calculating net-gradient ...')
+    locs_noise['net_gradient'] = netgradient_spots(spots[0])
+    
+    return locs_noise, spots
 
 #%%
 def gauss2D_on_mesh(p, xi, yi):
     arg = -(np.square(xi - p[:,1,np.newaxis,np.newaxis]) + np.square(yi - p[:,2,np.newaxis,np.newaxis]))
-    arg /=  2 * np.square(p[:,3,np.newaxis,np.newaxis])
-    y = p[:,0,np.newaxis,np.newaxis] * np.exp(arg) + p[:,4,np.newaxis,np.newaxis]
+    arg /=  2 * p[:,3,np.newaxis,np.newaxis] * p[:,4,np.newaxis,np.newaxis]
+    y = p[:,0,np.newaxis,np.newaxis] * np.exp(arg) + p[:,5,np.newaxis,np.newaxis]
 
     return y
 
 #%%
 def generate_spots(locs, box, e_tot, snr, sigma):
+    
+    print('Generating spots ... ')
     n_spots = len(locs)
     
     ### Generate coordinates for one spot
@@ -213,13 +255,14 @@ def generate_spots(locs, box, e_tot, snr, sigma):
     j_spots = np.repeat(j.reshape(1,box,box),n_spots,axis=0)
     
     ### Generate true parameters for all spots
-    p_spots = np.ones((n_spots,5),dtype = np.float32)
-    p_spots[:,0] = e_tot * locs['imagers'] # Assign total e-
-    p_spots[:,0] /= 2*np.pi # Convert to amplitude
-    p_spots[:,1] = (box-1)/2
-    p_spots[:,2] = (box-1)/2
-    p_spots[:,3] = sigma
-    p_spots[:,4] = e_tot / (2*np.pi*sigma**2*snr)
+    p_spots = np.ones((n_spots,6),dtype = np.float32)
+    p_spots[:,0] = e_tot * locs['imagers']              # Assign total e-
+    p_spots[:,0] /= 2*np.pi*sigma**2                    # Convert to amplitude
+    p_spots[:,1] = (box-1)/2                                  # Position exactly in center
+    p_spots[:,2] = (box-1)/2                      
+    p_spots[:,3] = sigma                                       # Same width in x and y direction!
+    p_spots[:,4] = sigma                                       
+    p_spots[:,5] = e_tot / (2*np.pi*sigma**2*snr) # Background = amplitde/snr!
     
     ### Generate gauss2D spots without noise
     spots = gauss2D_on_mesh(p_spots, i_spots, j_spots)
@@ -234,7 +277,6 @@ def generate_spots(locs, box, e_tot, snr, sigma):
     spots_readnoise = np.random.normal(0,np.sqrt(spots_readvar))
     
     ### Combine noise distributions
-    # spots_noise = spots_shotnoise
     spots_noise = spots_shotnoise + spots_readnoise
     spots_noise = spots_noise.astype(np.float32)
     
@@ -246,21 +288,63 @@ def generate_spots(locs, box, e_tot, snr, sigma):
     spots_shotnoise = spots_shotnoise - spots
     spots_readnoise = spots_readnoise.reshape(n_spots,box,box)
     
-    return spots_noise, spots_readvar, spots, spots_shotnoise, spots_readnoise
+    return [spots_noise, spots_readvar, spots, spots_shotnoise, spots_readnoise]
 
 #%%
-def fit_spots(locs,box,spots,spots_readvar):
+def fit_spots(locs,box,spots,spots_readvar, use_weight):
     locs_out = locs.copy()
+    init_shape = np.shape(spots)
+    
     if gpufit_available:
-            print('Weighted least square fitting (GPU) ...')
-            theta = localize.weightfit_spots_gpufit(spots,spots_readvar)
+            if use_weight:
+                print('Weighted least square fitting (GPU) ...')
+                theta = localize.weightfit_spots_gpufit(spots,spots_readvar)
+                ### Reshape spots after fitting
+                spots.shape = init_shape
+                spots_readvar.shape = init_shape
+            else:
+                print('Non-weighted least square fitting (GPU) ...')
+                theta = gausslq.fit_spots_gpufit(spots)
+                ### Reshape spots after fitting
+                spots.shape = init_shape
+            
+            ### Convert fit parameters to final output
             locs_out['photons'] = theta[:,0]
             locs_out['bg'] = theta[:,-1]
-            locs_out['x'] += theta[:,1] - int(box/2)
-            locs_out['y'] += theta[:,2] - int(box/2)
+            locs_out['x'] = locs_out['x_in'] + theta[:,1] - int(box/2)
+            locs_out['y'] = locs_out['y_in'] + theta[:,2] - int(box/2)
             locs_out['sx'] = theta[:,3]
             locs_out['sy'] = theta[:,4]
+            
     else:
         print('Only GPU fitting implemented assigning imagers ...')
     
     return locs_out
+
+#%%
+@numba.jit(nopython=True, nogil=True, cache=False)
+def netgradient_spots(spots):
+    box = np.shape(spots)[1]
+    box_half = int(box / 2)
+    
+    ### We create some kind of a meshgrid
+    ux = np.zeros((box, box), dtype=np.float32)
+    uy = np.zeros((box, box), dtype=np.float32)
+    for i in range(box):
+        val = box_half - i
+        ux[:, i] = uy[i, :] = val
+    unorm = np.sqrt(ux ** 2 + uy ** 2)
+    ux /= unorm
+    uy /= unorm
+     
+    ### Loop through spots and calculate net-gradient
+    ng = np.zeros(len(spots), dtype=np.float32)
+    for spot_idx, spot in enumerate(spots):
+        for i in range(1,box-1):
+            for j in range(1,box-1):
+                if not (i == box_half and j == box_half):
+                    gj = spot[j + 1, i] - spot[j - 1, i]
+                    gi = spot[j, i + 1] - spot[j, i - 1]
+                    ng[spot_idx] += (gj * uy[j,i] + gi * ux[j,i])
+                    
+    return ng
