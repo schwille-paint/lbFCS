72b6acf31e6f64b05b43a61617223f452976d8a3
Florian Stehr <florian.stehr@gmail.com>
2021-02-10 09:45:00 +0100

    eps_direct added to props, will be tested in simulations

--- lbfcs/pickprops.py
+++ lbfcs/pickprops.py
@@ -1,6 +1,7 @@
 # Import modules
 import os
 import numpy as np
+import numba
 import scipy.optimize
 import scipy.spatial
 import importlib
@@ -353,6 +354,43 @@ def props_qpaint(df,ignore,mode='ralf'):
     return s_out
 
 #%%
+def extract_eps_direct(p):
+    
+    ### Get histogram of single photon counts
+    y = np.histogram(p,800,(0,4000))[0]  # Only values returned in numba
+    x = np.arange(2.5,4000,5)                   # These are the bins
+    
+    ### Get histogram of double photon counts
+    y2 = np.histogram(2*p,800,(0,4000))[0]  # Only values returned in numba
+    y2 = y2 * 1.5                                              # Multiply values to roughly equal heights!!!
+    
+    y = y.astype(np.float32)
+    y2 = y2.astype(np.float32)
+    ### Smooth substraction using lagtimes
+    ### i.e. subtracting doubled photon histograms over and over by moving it to the right
+    y_diff = y.copy()
+    y2_lag = y2.copy()
+    for l in range(0,100):
+        y_diff -= y2_lag                          # Substract y2_lag from y
+        y2_lag = np.append(0,y2_lag)   # Add zero to start
+        y2_lag = y2_lag[:-1]                   # Remove last entry 
+    
+    y_diff[y_diff<0] = 0                        # Asign zero to all negative entries after smooth substraction
+    
+    ### Calculate mean of y_diff
+    eps_mean = np.sum(x * y_diff) / np.sum(y_diff)
+    
+    ### Calculate median of y_diff
+    y_diff_cum = np.cumsum(y_diff / np.sum(y_diff))
+    median_idx = np.argmin(np.abs(y_diff_cum-0.5))
+    eps_median = x[median_idx]
+    
+    ### Which value to return as eps
+    eps = (eps_median + eps_mean)/2
+    
+    return eps ,x, y, y2, y_diff
+
+#%%
 def props_other(df,NoFrames):
     '''
     Get other properties.
@@ -374,6 +412,10 @@ def props_other(df,NoFrames):
     
     ### Add field std_frame
     s_out['std_frame']=df['frame'].std(ddof=0) # Biased standard deviation
+    
+    ### Compute eps directly by smoothly subtracting doubled photon histogram from original photon histogram
+    photons = df.photons_ck.values                           # Use Chung-Kennedy filtered photon values
+    s_out['eps_direct'] = extract_eps_direct(photons)[0]
     
     return s_out
 
